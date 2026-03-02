d9f2f6fcf29fbe4b0d26af3337ab28522c319ddf
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2026-03-02 23:04:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,27 +16,36 @@ git clone --depth=1 https://github.com/linkease/nas-packages  package/nas
 git clone --depth=1 https://github.com/linkease/nas-packages-luci  package/nas-luci
 
 # 科学插件
-git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall  package/passwall
-git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall2  package/passwall2
-# git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall-packages  package/passwall-packages
-git clone --depth=1 -b dev https://github.com/vernesong/OpenClash  package/OpenClash
-git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki  package/nikki
-git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-momo  package/momo
-git clone --depth=1 -b kix https://github.com/QiuSimons/luci-app-daed  package/daed
-git clone --depth=1 -b master https://github.com/fw876/helloworld  package/helloworld
+# Passwall 
+# echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git' >> feeds.conf.default
+echo 'src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall.git' >> feeds.conf.default
+echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git' >> feeds.conf.default
+# OpenClash (指定 dev 分支)
+echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
+# Nikki / Momo
+echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git' >> feeds.conf.default
+echo 'src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git' >> feeds.conf.default
+# Daed (指定 kix 分支)
+echo 'src-git daed https://github.com/QiuSimons/luci-app-daed.git;kix' >> feeds.conf.default
+# Helloworld (SSR+)
+echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 
-# 功能插件
-git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/lucky
-git clone --depth=1 https://github.com/sbwml/luci-app-openlist2 package/openlist2
-
-git clone --depth=1 https://github.com/sirpdboy/luci-app-watchdog package/watchdog
-git clone --depth=1 https://github.com/sirpdboy/luci-app-taskplan package/taskplan
-git clone --depth=1 https://github.com/iv7777/luci-app-authshield package/authshield
-
-git clone --depth=1 https://github.com/EasyTier/luci-app-easytier package/easytier
-git clone --depth=1 https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community package/tailscale
-
-git clone --depth=1 https://github.com/isalikai/luci-app-owq-wol package/luci-app-owq-wol
+# === 功能插件 Feeds ===
+# Lucky (大吉大利，端口转发/反向代理神器)
+echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >> feeds.conf.default
+# Openlist2 (可能是指 OpenList 或相关列表工具)
+echo 'src-git openlist2 https://github.com/sbwml/luci-app-openlist2.git' >> feeds.conf.default
+# Sirpdboy 的插件 (看门狗 & 计划任务)
+echo 'src-git watchdog https://github.com/sirpdboy/luci-app-watchdog.git' >> feeds.conf.default
+echo 'src-git taskplan https://github.com/sirpdboy/luci-app-taskplan.git' >> feeds.conf.default
+# Authshield (认证屏蔽/管理)
+echo 'src-git authshield https://github.com/iv7777/luci-app-authshield.git' >> feeds.conf.default
+# EasyTier (内网穿透组网)
+echo 'src-git easytier https://github.com/EasyTier/luci-app-easytier.git' >> feeds.conf.default
+# Tailscale Community (注意：改名为 tailscale_community 以防与官方 packages 里的 tailscale 混淆)
+echo 'src-git tailscale_community https://github.com/Tokisaki-Galaxy/luci-app-tailscale-community.git' >> feeds.conf.default
+# OWQ WOL (网络唤醒)
+echo 'src-git owq_wol https://github.com/isalikai/luci-app-owq-wol.git' >> feeds.conf.default
 
 # 主题
 echo 'src-git aurora https://github.com/eamonxg/luci-theme-aurora.git' >> feeds.conf.default
